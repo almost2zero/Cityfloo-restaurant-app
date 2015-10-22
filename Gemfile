963de92d6d0138411a221120d7516256bd6cde41
Ryan Oakley <ryanoakley@Ryans-MacBook-Pro.local>
2015-10-21 21:45:42 -0400

    add images with carrierwave, s3, and figaro

--- Gemfile
+++ Gemfile
@@ -44,5 +44,8 @@ group :development do
   gem 'spring'
   gem 'rails_12factor', group: :production
   gem 'pg', group: :production
+  gem 'carrierwave'
+  gem "fog"
+  gem "figaro"
 end
 
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|                        
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["aws_access_key_id"],                        # keys saved in numnbers
+    aws_secret_access_key: ENV["aws_secret_access_key"],                        # keys saved in numnbers
+  }
+  config.fog_directory  = ENV["fog_directory"]                         # bucket name
+end
